9db7f62619d8cae01c9d3bd4664f63aef56b8b38
Tyler Davenport <tydavenport0415@gmail.com>
2024-12-31 12:36:37 -0500

    part one

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,60 @@
+/* eslint-disable consistent-return */
+/* eslint-disable lines-around-directive */
 'use client';
+/* eslint-disable spaced-comment */
 
- // any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
+// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
 
-import { Button } from 'react-bootstrap';
-import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
+//import { Button } from 'react-bootstrap';
+//import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
 import { useAuth } from '@/utils/context/authContext';
+import { useEffect, useState } from 'react';
 
 function Home() {
+  const [uselessFact, setUselessFact] = useState({});
   const { user } = useAuth();
 
+  const fetchFact = async () => {
+    try {
+      const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
+      const fact = await response.json();
+      setUselessFact(fact);
+    } catch (error) {
+      console.error('Error fetching useless fact:', error);
+    }
+  };
+
+  const selectResponse = (boolean) => {
+    if (!user) {
+      console.error('User is not authenticated');
+      return;
+    }
+
+    const obj = {
+      userId: user.uid,
+      permalink: uselessFact.permalink,
+      response: boolean,
+    };
+    fetchFact();
+    return obj;
+  };
+
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      <p>{uselessFact.text || 'Loading a useless fact...'}</p>
+
+      <h1>Did you know this fact?</h1>
+      <button className="btn btn-success" type="button" onClick={() => selectResponse(true)}>
+        Yes
+      </button>
+      <button className="btn btn-danger" type="button" onClick={() => selectResponse(false)}>
+        No
+      </button>
+    </>
   );
 }
 
